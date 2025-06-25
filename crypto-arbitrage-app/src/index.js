9008e7178b632bb56c6cf3801a34e90054131a5d
alpvm06 <102582920+alpvm06@users.noreply.github.com>
2025-06-25 13:47:17 +0000

    Uygulama dosyaları çıkarıldı ve yüklendi

---
+++ crypto-arbitrage-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import CryptoArbitrage from './CryptoArbitrage';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CryptoArbitrage />
+  </React.StrictMode>
+);
