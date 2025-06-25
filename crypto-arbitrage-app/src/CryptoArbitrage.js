2db43b1a623bed7574eaa33334efb9f70eceec57
alpvm06 <102582920+alpvm06@users.noreply.github.com>
2025-06-25 17:04:18 +0300

    Update CryptoArbitrage.js

---
+++ crypto-arbitrage-app/src/CryptoArbitrage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CryptoArbitrage = () => {
+  return (
+    <div>
+      <h1>Crypto Arbitrage App</h1>
+      <p>Farklı borsalar arası fiyat farklarını buradan takip edebilirsiniz.</p>
+    </div>
+  );
+};
+
+export default CryptoArbitrage;
